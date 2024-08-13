0547c2519356fa494416a67e30284d306776b0df
André Luiz Thiel <andre_thiel007@hotmail.com>
2024-08-13 15:22:31 -0300

    ajustes

--- POC-Global-9/POC.Web/Controllers/HomeController.cs
+++ POC-Global-9/POC.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using POC.Negocio.ViewModels;
 using System.Diagnostics;
+using System.Text;
 
 namespace POC.Web.Controllers
 {
@@ -13,8 +14,21 @@ namespace POC.Web.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            using StringContent jsonContent = new(
+                System.Text.Json.JsonSerializer.Serialize(new
+                {
+                    token = "db012306-a243-470d-9677-2d0261958aa1",
+                    number = "5541999143949",
+                    message = "write code sample"
+                }),
+                Encoding.UTF8, "application/json");
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.PostAsync("https://api.whatsprofissional.com/v1/api/sendMessage", jsonContent);
+            client.Dispose();
             return View();
         }
     }
